2c602b6bc2ebdbf1e5561e39f5676ce0b4c47345
Max Greenwald <powerslab3@gmail.com>
2024-06-25 19:09:04 -0400

    Updates

---
+++ precache-manifest.48970ef857f2a106a971b2771c3b7833.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b54e8792c07417d9e1a",
+    "url": "/vch_cb_1/static/css/main.7bccc1c6.chunk.css"
+  },
+  {
+    "revision": "7b54e8792c07417d9e1a",
+    "url": "/vch_cb_1/static/js/main.bc2aa827.chunk.js"
+  },
+  {
+    "revision": "dbffb7a61789111b0e52",
+    "url": "/vch_cb_1/static/js/runtime~main.7b96e597.js"
+  },
+  {
+    "revision": "6920c5f40e1b96932fbc",
+    "url": "/vch_cb_1/static/js/2.a5c69e8f.chunk.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/vch_cb_1/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "b070f1811207348f54b4988fc14bc950",
+    "url": "/vch_cb_1/static/media/rating_keydown_1.b070f181.png"
+  },
+  {
+    "revision": "14052abb8ad3bf096d71788ba2e2ace2",
+    "url": "/vch_cb_1/static/media/rating_keydown_2.14052abb.png"
+  },
+  {
+    "revision": "bc40f6358057323efebef6ea2db6cb59",
+    "url": "/vch_cb_1/static/media/rating_keydown_3.bc40f635.png"
+  },
+  {
+    "revision": "852779dc3fb4af1fd92cb2611fe82806",
+    "url": "/vch_cb_1/static/media/rating_keydown_4.852779dc.png"
+  },
+  {
+    "revision": "ae6e6c1fe3fef7ab5aa19a93442d23d3",
+    "url": "/vch_cb_1/static/media/rating_keydown_5.ae6e6c1f.png"
+  },
+  {
+    "revision": "4aa4e004ce46b0c3e1ac1200035c202b",
+    "url": "/vch_cb_1/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vch_cb_1/precache-manifest.48970ef857f2a106a971b2771c3b7833.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/vch_cb_1/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
